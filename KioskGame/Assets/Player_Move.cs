6d4b18f7d055bf61953c6805edbb5dfc43029e1c
brooksjt943 <brooksjt943@mail.sandhills.edu>
2020-01-21 15:53:48 -0500

    Added Assets
    Uploaded some free 2D assets to get started

---
+++ KioskGame/Assets/Player_Move.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 100;
+    public float moveX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player_Move();
+    }
+
+    void Player_Move()
+	{
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX < 0.0f && facingRight == false)
+		{
+            FlipPlayer();
+		}
+        else if(moveX > 0.0f && facingRight == true)
+		{
+            FlipPlayer();
+		}
+        //Animations
+        //Player Direction
+        //Physics
+        gameObject.GetComponent<RigidBody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<RigidBody2D>().velocity.y);
+	}
+
+    void Jump()
+	{
+        //Jumping code
+	}
+    void FlipPlayer()
+	{
+
+	}
+}
