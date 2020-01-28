edd7b7a2caa5ef97f32c17c307df46084c40e322
brooksjt943 <brooksjt943@mail.sandhills.edu>
2020-01-28 13:12:02 -0500

    Added player jump scripts

---
+++ KioskGame/Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public playerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void makePlayerJump()
+    {
+        //    player = FindObjectOfType<playerController> ();
+        player.jump();
+
+
+    }
+}
---
+++ KioskGame/Assets/Scripts/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rig;
+    public float jumpHeight;
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    public void jump()
+    {
+
+        rig.velocity = new Vector2(0, jumpHeight);
+        //or you can use addforce
+    }
+}
