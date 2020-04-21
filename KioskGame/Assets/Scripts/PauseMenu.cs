26290b36d6536a3d24c13a68e5eef79b48b18ff0
brooksjt943 <brooksjt943@mail.sandhills.edu>
2020-04-21 09:35:27 -0400

    Added Pause Menu

---
+++ KioskGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject PauseMenuUI;
+
+    void Start()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartScreen");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
